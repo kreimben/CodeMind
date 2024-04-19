4cc35c7b5ece9f520a9513d40c7ddc7899da8d8f
Jehwan Kim <aksidion@kreimben.com>
2024-04-19 17:17:27 +0900

    Refactor get_completion function in utils.py
    The get_completion function was overhauled to use a more structured pipeline. The new implementation swaps the model and tokenizer parameters for a unified pipeline parameter, leading to a cleaner, more efficient way to handle text generation and conversations. The special tokens handling is also improved.

--- utils.py
+++ utils.py
@@ -1,9 +1,17 @@
-def get_completion(query: str, model, tokenizer, system_prompt='', device='cuda:0', max_new_tokens=512) -> str:
-    prompt = f'<bos><start_of_turn>user\n{system_prompt} \n\n{query} <end_of_turn>\n<start_of_turn>model'
-
-    inputs = tokenizer(prompt, return_tensors="pt", padding=True, truncation=True, add_special_tokens=True).to(device)
-
-    generated_ids = model.generate(**inputs, max_length=500, num_return_sequences=1)
-
-    decoded = tokenizer.decode(generated_ids[0], skip_special_tokens=True)
-    return decoded
+def get_completion(query: str, pipeline, system_prompt='') -> str:
+    messages = [
+        {
+            "role": "user",
+            "content": f"{system_prompt}\n\n{query}",
+        }
+    ]
+    prompt = pipeline.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+    outputs = pipeline(
+        prompt,
+        do_sample=True,
+        temperature=0.2,
+        top_k=50,
+        top_p=0.95,
+        add_special_tokens=True
+    )[0]['generated_text']
+    return outputs
